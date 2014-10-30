c901b43bc37aaac8c6e8f10b5ac70c7733e5f3bd
brian.berzellini <berzelba@Macintosh.local>
2014-10-30 08:11:51 -0400

    ajax changes 10-30-14

--- js/index.js
+++ js/index.js
@@ -1,50 +1,73 @@
+$.get('Talent-table.csv', function (data) {
+	console.log(data);
+	var html = '';
+	var creatives = $.csv.toObjects(data);
 
-var html = '';
-var creatives = [
-		{
-			name: 'Brian',
-			title:'Recruiter',
-			blurb:'Lorem ipsum dolor sit amet, dicam intellegebat vel ad, cu per facer tantas repudiandae. Mea ludus nonumes elaboraret ne. Ea alterum fuisset suscipiantur sit, utinam copiosae et eum. Pro in eripuit erroribus, et sea ridens tritani, his choro nullam dissentiunt et. Ad sea amet laboramus, ad eum altera suscipit. Nec natum audire oblique ad, ius tota periculis ad, tale periculis sed ex. In sea dico justo expetenda, vel quod clita comprehensam ex, vel sint oporteat ut.',
-		},{
-			name: 'Wade',
-			title:'Responsive Guru',
-			blurb:'Lorem ipsum dolor sit amet, dicam intellegebat vel ad, cu per facer tantas repudiandae. Mea ludus nonumes elaboraret ne. Ea alterum fuisset suscipiantur sit, utinam copiosae et eum. Pro in eripuit erroribus, et sea ridens tritani, his choro nullam dissentiunt et. Ad sea amet laboramus, ad eum altera suscipit. Nec natum audire oblique ad, ius tota periculis ad, tale periculis sed ex. In sea dico justo expetenda, vel quod clita comprehensam ex, vel sint oporteat ut.',
-		},{
-			name: 'Ryan',
-			title:'Hard Core Programmer',
-			blurb:'Lorem ipsum dolor sit amet, dicam intellegebat vel ad, cu per facer tantas repudiandae. Mea ludus nonumes elaboraret ne. Ea alterum fuisset suscipiantur sit, utinam copiosae et eum. Pro in eripuit erroribus, et sea ridens tritani, his choro nullam dissentiunt et. Ad sea amet laboramus, ad eum altera suscipit. Nec natum audire oblique ad, ius tota periculis ad, tale periculis sed ex. In sea dico justo expetenda, vel quod clita comprehensam ex, vel sint oporteat ut.',
-		}];
+	/*creatives 
+	[
+			{
+				name: 'Brian',
+				title:'Recruiter',
+				blurb:'Lorem ipsum dolor sit amet, dicam intellegebat vel ad, cu per facer tantas repudiandae. Mea ludus nonumes elaboraret ne. Ea alterum fuisset suscipiantur sit, utinam copiosae et eum. Pro in eripuit erroribus, et sea ridens tritani, his choro nullam dissentiunt et. Ad sea amet laboramus, ad eum altera suscipit. Nec natum audire oblique ad, ius tota periculis ad, tale periculis sed ex. In sea dico justo expetenda, vel quod clita comprehensam ex, vel sint oporteat ut.',
+			},{
+				name: 'Wade',
+				title:'Responsive Guru',
+				blurb:'Lorem ipsum dolor sit amet, dicam intellegebat vel ad, cu per facer tantas repudiandae. Mea ludus nonumes elaboraret ne. Ea alterum fuisset suscipiantur sit, utinam copiosae et eum. Pro in eripuit erroribus, et sea ridens tritani, his choro nullam dissentiunt et. Ad sea amet laboramus, ad eum altera suscipit. Nec natum audire oblique ad, ius tota periculis ad, tale periculis sed ex. In sea dico justo expetenda, vel quod clita comprehensam ex, vel sint oporteat ut.',
+			},{
+				name: 'Ryan',
+				title:'Hard Core Programmer',
+				blurb:'Lorem ipsum dolor sit amet, dicam intellegebat vel ad, cu per facer tantas repudiandae. Mea ludus nonumes elaboraret ne. Ea alterum fuisset suscipiantur sit, utinam copiosae et eum. Pro in eripuit erroribus, et sea ridens tritani, his choro nullam dissentiunt et. Ad sea amet laboramus, ad eum altera suscipit. Nec natum audire oblique ad, ius tota periculis ad, tale periculis sed ex. In sea dico justo expetenda, vel quod clita comprehensam ex, vel sint oporteat ut.',
+			}];
+*/
+	function generateMarketingBlurb(creative) {
 
-function generateMarketingBlurb(creative) {
+	var ret = ''
 
-var ret = ''
+		ret += '<div class="talent-slide-container">';
+		ret += '<div class="talent-slide">';
+		ret += '<div class="talent-content-container">';
+		ret += '<h1 class="creative-intro color-white Xlarge-font">HEY THERE, <span class="color-blue">I\'M</span>...</h1>';
+		ret += '<div class="creative-info">';
+		ret += '<h2 class="creative-name color-orange XXlarge-font">' + creative.Name + '</h2>';
+		ret += '<h3 class="creative-title color-white Xlarge-font">I\'M A <span class="color-blue">' + creative.Title + '</span></h3>';
+		ret += '<a class="open-blurb color-black medium-font">MORE</a>';
+		ret += '</div>';
+		ret += '<div class="creative-blurb-container medium-font color-white">';
+		ret += '<a class="close-blurb Xlarge-font color-black">+</a>';
+		ret += '<p class="creative-blurb regular-font">' + creative.Blurb + '</p>';
+		ret += '<a class="interested" href="mailto:sbenton@vitamintalent.com?Subject=' + creative.Name + '%20looks%20AWESOME!&body=Hi%20Shannon!">Interested!</a>';
+		ret += '</div>';
+		ret += '</div>';
+		ret += '</div>';
+		ret += '</div>';
+		return ret;
+	}
 
-ret += '<div class="talent-slide-container">';
-ret += '<div class="talent-slide">';
-ret += '<div class="talent-content-container">';
-ret += '<h1 class="creative-intro color-white Xlarge-font">HEY THERE, <span class="color-blue">I\'M</span>...</h1>';
-ret += '<div class="creative-info">';
-ret += '<h2 class="creative-name color-orange XXlarge-font">' + creative.name + '</h2>';
-ret += '<h3 class="creative-title color-white Xlarge-font">I\'M A <span class="color-blue">' + creative.title + '</span></h3>';
-ret += '<a class="open-blurb color-black medium-font">MORE</a>';
-ret += '</div>';
-ret += '<div class="creative-blurb-container medium-font color-white">';
-ret += '<a class="close-blurb Xlarge-font color-black">+</a>';
-ret += '<p class="creative-blurb regular-font">' + creative.blurb + '</p>';
-ret += '<a class="interested" href="mailto:sbenton@vitamintalent.com?Subject=' + creative.name + '%20looks%20AWESOME!&body=Hi%20Shannon!">Interested!</a>';
-ret += '</div>';
-ret += '</div>';
-ret += '</div>';
-ret += '</div>';
-return ret;
-}
+	for (var i = 0; i < creatives.length; i++) {
+		html += generateMarketingBlurb(creatives[i]);
+	}
 
-for (var i = 0; i < creatives.length; i++) {
-	html += generateMarketingBlurb(creatives[i]);
+	$('#slider').html(html);
 
-$('#slider').html(html);
+	$('.open-blurb').on('click', function() {
+		var creativeInfoBlurb = $(this).parent('.creative-info').next('.creative-blurb-container');
+		creativeInfoBlurb.toggleClass('open');
+	});
 
-};
+	$('.close-blurb').on('click', function() {
+		var creativeInfoBlurb = $(this).parent('.creative-blurb-container');
+		creativeInfoBlurb.removeClass('open');
+	});
+
+	$("#slider").owlCarousel({
+  		items: 1,
+  		singleItem: true,
+  		navigation: true,
+  		navigationText: ['<<','>>'],
+  		pagination: true,
+  		autoPlay: true
+  	});
+});
 
 	$('a[href*=#]:not([href=#])').click(function() {
  
@@ -60,31 +83,17 @@ $('#slider').html(html);
     }
   });
 
-	$('.open-blurb').on('click', function() {
-		var creativeInfoBlurb = $(this).parent('.creative-info').next('.creative-blurb-container');
-		creativeInfoBlurb.toggleClass('open');
-	});
-
-	$('.close-blurb').on('click', function() {
-		var creativeInfoBlurb = $(this).parent('.creative-blurb-container');
-		creativeInfoBlurb.removeClass('open');
-	});
-
-	$('.nav-responsive').on('click', function() {
-		var responsiveNavMenu = $('.responsive-menu')
+function toggleMenu(e) {
+	var responsiveNavMenu = $('.responsive-menu');
+	if ($(e.target).is('.nav-responsive, .nav-responsive-btn')) {
 		responsiveNavMenu.toggleClass('open-responsive-menu');
-	});
-
-$("#slider").owlCarousel({
-  		items: 1,
-  		singleItem: true,
-  		navigation: true,
-  		navigationText: ['<<','>>'],
-  		pagination: true,
-  		autoPlay: true
-  	});
+	} else {
+		responsiveNavMenu.removeClass('open-responsive-menu');
+	}
+};
 
 
-  
+
+  $(window).on('click', toggleMenu);
 
 
