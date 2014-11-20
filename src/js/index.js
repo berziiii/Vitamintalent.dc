f33f799132798b57a9432c385228eaeaa8d7fed2
brian.berzellini <berzelba@Macintosh.local>
2014-11-20 13:26:04 -0500

    stripped update.
    11-20-14 Stripped update.

--- src/js/index.js
+++ src/js/index.js
@@ -7,18 +7,39 @@ var creatives = $.csv.toObjects(data);
 var html = '';
 var creatives = [
 		{
-			Name: 'Brian',
-			Title:'Recruiter',
-			Blurb:'Lorem ipsum dolor sit amet, dicam intellegebat vel ad, cu per facer tantas repudiandae. Mea ludus nonumes elaboraret ne. Ea alterum fuisset suscipiantur sit, utinam copiosae et eum. Pro in eripuit erroribus, et sea ridens tritani, his choro nullam dissentiunt et. Ad sea amet laboramus, ad eum altera suscipit. Nec natum audire oblique ad, ius tota periculis ad, tale periculis sed ex. In sea dico justo expetenda, vel quod clita comprehensam ex, vel sint oporteat ut.',
+			Name: 'Teresa',
+			Title:'Front-End Developer',
+			Blurb:'Teresa is an experienced, self taught front-end developer who specializes in hand coding in HTML, CSS and PHP. She loves working collaboratively with teams and proactively looks for ways to improve. One of Teresa\'s successes includes her most recent time at a development organization in which she worked with a team on multiple fast-paced deadlines for mid-sized east coast companies. Teresa is confident in her ability to code standard compliant, cross-browser and mobile compatible XHTML/CSS with CSS3 and HTML5. She believes that the best design is clean design and develops her sites to be user-friendly web interfaces consisting a range of digital marketing materials. If you’re looking for a developer that can deliver quality through consistent attention to detail, then Teresa is your girl!',
 		},{
-			Name: 'Wade',
-			Title:'Responsive Guru',
-			Blurb:'Lorem ipsum dolor sit amet, dicam intellegebat vel ad, cu per facer tantas repudiandae. Mea ludus nonumes elaboraret ne. Ea alterum fuisset suscipiantur sit, utinam copiosae et eum. Pro in eripuit erroribus, et sea ridens tritani, his choro nullam dissentiunt et. Ad sea amet laboramus, ad eum altera suscipit. Nec natum audire oblique ad, ius tota periculis ad, tale periculis sed ex. In sea dico justo expetenda, vel quod clita comprehensam ex, vel sint oporteat ut.',
+			Name: 'Josh',
+			Title:'Writer/Designer',
+			Blurb:'Josh has a Bachelors degree in English and Creative Writing along with his Masters in Comparative History. Josh\'s last position was The Communications Manager where he managed all online and print communications. He led a team of 3 and worked with outside vendors of specific projects that he oversaw from inception to completion. Josh has a good background with communications and graphic design experience, which in return lets him understand the full process of each project and set strong deadlines.',
 		},{
-			Name: 'Ryan',
-			Title:'Hard Core Programmer',
-			Blurb:'Lorem ipsum dolor sit amet, dicam intellegebat vel ad, cu per facer tantas repudiandae. Mea ludus nonumes elaboraret ne. Ea alterum fuisset suscipiantur sit, utinam copiosae et eum. Pro in eripuit erroribus, et sea ridens tritani, his choro nullam dissentiunt et. Ad sea amet laboramus, ad eum altera suscipit. Nec natum audire oblique ad, ius tota periculis ad, tale periculis sed ex. In sea dico justo expetenda, vel quod clita comprehensam ex, vel sint oporteat ut.',
+			Name: 'Mike',
+			Title:'Writer/Editor',
+			Blurb:'Experienced writer and editor with a diverse background in newspaper, magazine, and online media. Driven by a dedicated work ethic, possesses top-notch communication skills and is capable of writing extensive, full-length features just as easily as clear, concise copy. Regularly performing under strict deadlines, has become an accomplished multi-tasker, oftentimes juggling numerous projects at once. Is a staunch grammar and spelling watchdog, able to easily explain when it\'s appropriate to use an apostrophe, as well as tell the differences between farther and further, affect and effect, and accept and except. Has a strong grasp of digital implementation in business; skilled in web publishing, content management, HTML formatting; and understands the duties of an admin on social media sites such as Facebook, Twitter, and Tumblr for marketing purposes. Dedicated to personal and professional development, with a history of perseverance',
+		},{
+			Name: 'Daoud',
+			Title:'Designer',
+			Blurb:'Daoud is a fantastic, multi talented designer with outstanding experience in both print and web. He has worked for several contracts such as major agencies, non-profits and advocacy groups and is currently wrapping up his time with a communications firm as their Senior Designer/Digital Coordinator. Daoud is the creative, versatile, and sharp designer with sound experience in infographics. His mastery of Adobe Creative Suite and strong design intuition would prove him to be a dependable and efficient candidate for this role.',
+		},{
+			Name: 'Brandon',
+			Title:'Web Developer',
+			Blurb:'Brandon is a Web Designer with over ten years of experience who possesses a unique skill set that combines creative talent, expert technical skills and first-rate interpersonal skills. Brandon would call himself a Web Designer with a highly trained eye for detail that loves the look of a highly sophisticated yet simple design. Brandon recently was with a member based non-profit organization that works to catalyze improvements in healthcare as their Senior Web Producer responsible for designing graphical user interface and collaborated with several teams on the functionality design for their web application, home page, and other templates built in Ektron CMS. Prior to this, Brandon comes from a Web Designer and Graphic designer background. Brandon is up on industry best practices, familiar with Drupal, Wordpress and other CMS’s, HTML, CSS, JavaScript, jQuery, AJAX, and PHP. Brandon’s strong track record of leading versatile teams and delivering high-quality design projects combined with his creative eye would make him a key candidate for this role. ',
+		},{
+			Name: 'Linda',
+			Title:'Visual Designer',
+			Blurb:'If you’re looking for a versatile and multifunctional web professional, Linda is your rock star! Linda is a Front-end developer that has outstanding User Interface experience and 10+ years of federal project knowledge. She has a strong understanding of HTML, CSS, JavaScript, and JQuery but don’t let her technical understanding fool you, she is a designer at heart! Linda has a range of transferable skills such as: graphic design, interface design, responsive design, and Section 508 accessibility. Currently, Linda has been working with a governmental entity as their Web Design Adviser where she specializes in designing and developing their web pages. Previously, she worked with the national health organization as a Graphic and Web Designer/Developer. Linda is passionate about User Interface design, the methodologies behind it, and constantly learning new skills to be the best designer she can be. Her design eye, technical web understanding, and health industry experience would prove her to be a must-have for this project!',
+		},{
+			Name: 'Joe',
+			Title:'Graphic Designer',
+			Blurb:'Joe is a Graphic Designer that is passionate about creating logos, branding, typography, web and mobile design. Prior to becoming a Designer, Joe was a professional journalist along with being a writer/editor. This became the gateway into design. Through journalism, Joe was able to create design focused on detail. He is constantly striving to grasp the audience with each design. Joe is creative and passionate about design, he has an eye for detail so that every project/assignment will be perfect.',
+		},{
+			Name: 'Daniel',
+			Title:'Web Content Manager',
+			Blurb:'Daniel is a web content specialist who has led email marketing campaigns, managed website designs/development and has developed analytical dashboards and reporting processes within a multitude of organizations. He has worked within a plethora of content management systems such as Drupal, WordPress and SharePoint and can even make HTML and CSS changes if needed. He is extremely results oriented and passionate about all the projects he works on and is eager to tackle complex problems. One of his biggest successes was when he worked for an integrated marketing firm in which he was tasked with learning Convio (which no one in the firm has used before) to create an e-commerce site for a nonprofit organization. Daniel worked collaboratively with the internal development team to make modifications to Convio\'s system and worked closely with the client to make sure they understand the complications that arose and managed their expectations in regards to deadlines. The e-store was launched successfully and Daniel was praised for his eagerness to take on the project and helped to lead the creation of additional Convio based sites for their clients.',
 		}];
+
 
 	function generateMarketingBlurb(creative) {
 
